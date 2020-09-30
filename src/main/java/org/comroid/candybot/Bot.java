40abab11f5c113e5c5b8c8c1f5404200be5a4219
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2020-09-30 11:56:53 +0200

    use botsuite

--- src/main/java/org/comroid/candybot/Bot.java
+++ src/main/java/org/comroid/candybot/Bot.java
@@ -1,50 +1,28 @@
 package org.comroid.candybot;
 
-import org.comroid.api.UUIDContainer;
-import org.comroid.candybot.bank.CandyBank;
+import org.comroid.botsuite.javacord.JavacordBotBase;
 import org.comroid.commandline.CommandLineArgs;
-import org.comroid.common.Disposable;
 import org.comroid.common.io.FileHandle;
-import org.comroid.javacord.util.commands.CommandHandler;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
-import java.util.concurrent.CompletableFuture;
-
-public final class Bot extends UUIDContainer.Base implements Disposable {
+public final class Bot extends JavacordBotBase {
+    public static final FileHandle DIR_DATA = new FileHandle("/srv/dcb/candybot/", true);
+    public static final FileHandle DIR_LOGIN = DIR_DATA.createSubDir("login");
     public static CommandLineArgs ARGS;
     public static Bot instance;
 
-    public final FileHandle dir;
-    public final FileHandle data;
-    public final CandyBank candybank;
-    public final DiscordApi discord;
-    public final CommandHandler cmd;
-
-    private Bot(FileHandle baseDir) {
-        this.dir = baseDir;
-        this.data = baseDir.createSubDir("data");
-
-        this.candybank = new CandyBank(data.createSubDir("vaults"));
-
-        this.discord = ARGS.process("token")
-                .or(data.createSubFile("discord.cred")::getContent)
-                .map(token -> new DiscordApiBuilder().setToken(token))
-                .map(DiscordApiBuilder::login)
-                .into(CompletableFuture::join);
-        this.cmd = new CommandHandler(discord);
-
-        addChildren(discord::disconnect, candybank);
+    private Bot(DiscordApi api) {
+        super(DIR_DATA, api);
     }
 
     public static void main(String[] args) {
         ARGS = CommandLineArgs.parse(args);
 
-        instance = ARGS.process("dir")
-                .or(() -> "/srv/dcb/candybot/")
-                .map(path -> new FileHandle(path, true))
-                .into(Bot::new);
-
-        Runtime.getRuntime().addShutdownHook(new Thread(instance::close));
+        DiscordApi api = new DiscordApiBuilder()
+                .setToken(ARGS.wrap("token").orElseGet(DIR_LOGIN.createSubFile("discord.cred")::getContent))
+                .login()
+                .join();
+        instance = new Bot(api);
     }
 }
