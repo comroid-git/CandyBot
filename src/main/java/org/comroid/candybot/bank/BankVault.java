d8c5c7c83c0bb494d310aa0bcb5649825f13d5f0
Tobias Burdow [Kaleidox] <burdoto@outlook.com>
2021-01-09 16:01:39 +0100

    meine kraft verlässt mich

--- src/main/java/org/comroid/candybot/bank/BankVault.java
+++ src/main/java/org/comroid/candybot/bank/BankVault.java
@@ -2,7 +2,7 @@ package org.comroid.candybot.bank;
 
 import org.comroid.candybot.Bot;
 import org.comroid.common.io.FileHandle;
-import org.comroid.uniform.ValueType;
+import org.comroid.uniform.node.impl.StandardValueType;
 import org.comroid.uniform.adapter.json.jackson.JacksonJSONAdapter;
 import org.comroid.varbind.FileConfiguration;
 import org.comroid.varbind.annotation.RootBind;
@@ -16,14 +16,14 @@ public final class BankVault extends FileConfiguration {
             = new GroupBind<>(JacksonJSONAdapter.instance, "bank-vault");
     public static final VarBind<BankVault, Long, Server, Server> guild
             = Root.createBind("guild")
-            .extractAs(ValueType.LONG)
+            .extractAs(StandardValueType.LONG)
             .andRemap(id -> Bot.instance.discord.getServerById(id).orElse(null))
             .onceEach()
             .setRequired()
             .build();
     public static final VarBind<BankVault, Boolean, Boolean, Boolean> usesGlobal
             = Root.createBind("usesGlobalVault")
-            .extractAs(ValueType.BOOLEAN)
+            .extractAs(StandardValueType.BOOLEAN)
             .asIdentities()
             .onceEach()
             .setRequired()
